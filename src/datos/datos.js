8b09571fdc13017d7ac1351f51716439bc7dc2fd
David Venté <davidventepolo@gmail.com>
2023-03-20 03:27:46 -0500

    20-Mar 01 ~ Ajustes para ServidorUbuntu

--- src/datos/datos.js
+++ src/datos/datos.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2/promise');
 
-const password = ''
+// const password = ''
+const password = 'contrasena123'
 // Crea una conexión a la base de datos
 // !!!! --- PAQUETES
 async function ingresarPaquete(producto) {
